2c52b61fca8e9e6481f363ed55f4902be43cbb56
zhangxiaoya <zhangxiaoya1989@gmail.com>
2017-03-23 10:31:41 +0800

    :yum: add kernel function for CUDA and call this kernel function in main

--- HelloCUDA/helloCUDA.cu
+++ HelloCUDA/helloCUDA.cu
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
+__global__ void helloFromGPU()
+{
+    printf("Hello World From GPU!\n");
+}
+
 int main()
 {
+    // helloFromGPU
     printf("Hello World From CPU!\n");
+
+    // helloFromGPU
+    helloFromGPU<<<1, 10>>>();
+    cudaDeviceReset();
+    return 0;
 }
