bcfd55bd2a62a43fabfca798da9148c78d751c92
zhangxiaoya <zhangxiaoya1989@gmail.com>
2017-07-29 16:01:23 +0800

    do something

--- programmingModel/checkDimension.cu
+++ programmingModel/checkDimension.cu
@@ -3,8 +3,11 @@
 
 __global__ void checkIndex(void)
 {
-    printf("threadIdx: (%d, %d, %d) blockIdx: (%d, %d, %d) blockDim: (%d, %d, %d) gridDim: (%d, %d, %d)\n", threadIdx.x, threadIdx.y,
-            threadIdx.z, blockIdx.x, blockIdx.y, blockIdx.z, blockDim.x, blockDim.y, blockDim.z, gridDim.x, gridDim.y, gridDim.z);
+    printf("threadIdx: (%d, %d, %d) blockIdx: (%d, %d, %d) blockDim: (%d, %d, %d) gridDim: (%d, %d, %d)\n",
+    threadIdx.x, threadIdx.y, threadIdx.z,
+    blockIdx.x, blockIdx.y, blockIdx.z,
+    blockDim.x, blockDim.y, blockDim.z,
+    gridDim.x, gridDim.y, gridDim.z);
 }
 
 int main()
