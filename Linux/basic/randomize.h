c5a6d60642cbc7f7efa19d3bffcdde3c77190107
zhangxiaoya <zhangxiaoya1989@gmail.com>
2018-07-10 15:51:44 +0800

    use cmake for cuda on ubuntu16

---
+++ Linux/basic/randomize.h
@@ -0,0 +1,7 @@
+
+#ifndef __randomize_h__
+#define __randomize_h__
+
+void randomize(float& x, float& y, float& z);
+
+#endif
---
+++ Linux/basic/v3.h
@@ -0,0 +1,22 @@
+
+#ifndef __v3_h__
+#define __v3_h__
+
+#include "JETBRAIN_IDE.h"
+
+class v3
+{
+public:
+	float x;
+	float y;
+	float z;
+	
+	v3();
+	v3(float xIn, float yIn, float zIn);
+	void randomize();
+	__host__ __device__ void normalize();
+	__host__ __device__ void scramble();
+
+};
+
+#endif
